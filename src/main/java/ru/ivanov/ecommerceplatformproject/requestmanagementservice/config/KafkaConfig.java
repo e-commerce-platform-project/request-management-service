cbdc387c1a4a31c844af73a830c5d1cf60d06208
Anton Ivanov <ivanova21600@mail.ru>
2025-11-24 22:39:05 +0300

    add KafkaConfig and SchedulingConfig

---
+++ src/main/java/ru/ivanov/ecommerceplatformproject/requestmanagementservice/config/KafkaConfig.java
@@ -0,0 +1,41 @@
+package ru.ivanov.ecommerceplatformproject.requestmanagementservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+@RequiredArgsConstructor
+public class KafkaConfig {
+
+    private final Environment environment;
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getRequiredProperty("spring.kafka.bootstrap-servers"));
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JacksonJsonSerializer.class);
+        config.put(ProducerConfig.ACKS_CONFIG, environment.getRequiredProperty("spring.kafka.producer.acks"));
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true);
+        config.put(JacksonJsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
---
+++ src/main/java/ru/ivanov/ecommerceplatformproject/requestmanagementservice/config/SchedulingConfig.java
@@ -0,0 +1,9 @@
+package ru.ivanov.ecommerceplatformproject.requestmanagementservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
